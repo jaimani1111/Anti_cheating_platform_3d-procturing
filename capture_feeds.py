fe057102324a395347d27f7e7bb7ec60c305689b
jaimani1111 <113229601+jaimani1111@users.noreply.github.com>
2025-01-25 19:48:18 +0530

    Update capture_feeds.py

--- capture_feeds.py
+++ capture_feeds.py
@@ -1,9 +1,8 @@
 import cv2
 
-# Capture video from laptop camera
+
 cap_laptop = cv2.VideoCapture(0)
 
-# Capture video from phone camera (use IP Webcam app on phone for this example)
 cap_phone = cv2.VideoCapture("http://192.168.1.26:8080/video")
 
 while True:
@@ -11,11 +10,11 @@ while True:
     ret2, frame2 = cap_phone.read()
     
     if ret1 and ret2:
-        # Display the frames
+        
         cv2.imshow('Laptop Camera', frame1)
         cv2.imshow('Phone Camera', frame2)
         
-        # Break the loop on 'q' key press
+        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
