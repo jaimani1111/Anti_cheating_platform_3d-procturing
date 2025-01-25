a387127a712e0b558e3c2bbaf6c8ec2045ff64f7
jaimani1111 <113229601+jaimani1111@users.noreply.github.com>
2025-01-25 19:49:22 +0530

    Update facial_recognition.py

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,11 +1,11 @@
 import cv2
 import dlib
 
-# Load pre-trained models for face detection and shape prediction
+
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-# Capture video from laptop camera
+# laptop camera
 cap_laptop = cv2.VideoCapture(0)
 
 while True:
@@ -19,7 +19,7 @@ while True:
             cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
             landmarks = predictor(gray, face)
             
-            for n in range(36, 48):  # Eye landmarks range
+            for n in range(36, 48):  
                 x = landmarks.part(n).x
                 y = landmarks.part(n).y
                 cv2.circle(frame1, (x, y), 2, (255, 0, 0), -1)
